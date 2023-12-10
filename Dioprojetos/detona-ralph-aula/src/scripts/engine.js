64ccd0ea015d9804a241668c525cf7e76ea18d90
JorgeMario115 <117872735+JorgeMario115@users.noreply.github.com>
2023-12-10 15:14:25 -0300

    add Desafios da Dio ,Jogo da memória e Detona Halph.

---
+++ Dioprojetos/detona-ralph-aula/src/scripts/engine.js
@@ -0,0 +1,79 @@
+const state = {
+    view: {
+        squares: document.querySelectorAll(".square"),
+        enemy: document.querySelector(".enemy"),
+        timeLeft: document.querySelector("#time-left"),
+        score: document.querySelector("#score"),
+    },
+
+    values: {
+        // timerId: null,
+        gameVelocity: 1000,
+        hitPosition: 0,
+        result: 0,
+        currentTime: 60,
+    },
+    actions: {
+        timerId: setInterval(randomSquare, 1000),
+        countDownTimerId: setInterval(countDown, 1000),
+
+    }
+    
+};
+
+function countDown() {
+    state.values.currentTime--;
+    state.view.timeLeft.textContent = state.values.currentTime;
+
+    if(state.values.currentTime <= 0) {
+        clearInterval(state.actions.countDownTimerId);
+        clearInterval(state.actions.timerId);
+        alert("Game Over! O seu resultado foi: "+ state.values.result);
+    }
+}
+
+function playSound() {
+    audio = new Audio(``);
+    audio.src = "";
+    audio.volume = 0.2;
+    audio.play();
+}
+
+function randomSquare() {
+    state.view.squares.forEach((square) => {
+        square.classList.remove("enemy");
+    });
+
+    
+    let randomNumber = Math.floor(Math.random() * 9);
+    let randomSquare = state.view.squares[randomNumber];
+    randomSquare.classList.add("enemy");
+
+    state.values.hitPosition = randomSquare.id;
+}
+
+// function moveEnemy() {
+//     state.values.timerId = setInterval(randomSquare, state.values.gameVelocity);
+// }
+
+function addListenerHitBox() {                                    // Listener é alguem que espera uma ação
+    state.view.squares.forEach((square) => {
+        square.addEventListener("mousedown", () => {
+            if(square.id === state.values.hitPosition) {
+                state.values.result++;
+                state.view.score.textContent = state.values.result;
+                state.values.hitPosition = null;
+                playSound();
+            }
+            
+        });
+    });  // foreEch ( cada um dos squares)
+}
+
+function init() { 
+   
+    // moveEnemy();               movendo o inimigo pelo state.actions
+    addListenerHitBox();
+}  
+
+init();
